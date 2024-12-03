e51436aefcf66670c1b86bd042ea2a204ddf1fee
Paul Mac <plipp2804@gmail.com>
2024-12-02 21:32:59 -0800

    dec 2 done

--- 2024-12-02/main.go
+++ 2024-12-02/main.go
@@ -14,64 +14,86 @@ func handle_err(e error) {
   }
 }
 
+func is_safe(list []string) bool {
+  var decreasing bool
+
+  if len(list) < 2 {
+    return true
+  }
+
+  parsed_first, err := strconv.Atoi(list[0])
+  handle_err(err)
+
+  parsed_second, err := strconv.Atoi(list[1])
+  handle_err(err)
+
+  if parsed_first < parsed_second {
+    decreasing = false
+  } else {
+    decreasing = true
+  }
+
+  for i := 0; i < len(list) - 1; i++ {
+    parsed_curr, err := strconv.Atoi(list[i])
+    handle_err(err)
+
+    parsed_next, err := strconv.Atoi(list[i+1])
+    handle_err(err)
+
+    if decreasing {
+      val := parsed_curr - parsed_next
+      if val < 1 || val > 3 {
+        return false
+      }
+    } else {
+      val := parsed_next - parsed_curr
+      if val < 1 || val > 3 {
+        return false
+      }  
+    }
+  }
+
+  return true
+}
+
 func main() {
-  // get data from file
   file, err := os.Open("inputs.txt")
   handle_err(err)
 
   defer file.Close()
 
-  // scan line-by-line and add values to lists
   scanner := bufio.NewScanner(file)
 
   safe := 0
+  safe_dampened := 0
 
   for scanner.Scan() {
     line := scanner.Text()
     parsed_line := strings.Split(line, " ")
-  
-    var decreasing bool
-
-    parsed_first, err := strconv.Atoi(parsed_line[0])
-    handle_err(err)
-
-    parsed_second, err := strconv.Atoi(parsed_line[1])
-    handle_err(err)
-
-    if parsed_first < parsed_second {
-      decreasing = false
+    fmt.Println(parsed_line) 
+    if is_safe(parsed_line) {
+      safe += 1
     } else {
-      decreasing = true
-    }
+      for i := 0; i < len(parsed_line); i++ {
+        new_list := append([]string(nil), parsed_line[:i]...)
+        new_list = append(new_list, parsed_line[i+1:]...)
 
-    for i := 0; i < len(parsed_line) - 1; i++ {
-      parsed_curr, err := strconv.Atoi(parsed_line[i])
-      handle_err(err)
-
-      parsed_next, err := strconv.Atoi(parsed_line[i+1])
-      handle_err(err)
-
-      if decreasing {
-        val := parsed_curr - parsed_next
-        if val < 1 || val > 3 {
-          safe -= 1
-          break
+        if is_safe(new_list) {
+            safe_dampened += 1
+            fmt.Println("=== old list was:", parsed_line, "\n=== new safe list:", new_list)
+            break
+        } else {
+            fmt.Println("=== old list was:", parsed_line, "\n=== new unsafe list:", new_list)
         }
-      } else {
-        val := parsed_next - parsed_curr
-        if val < 1 || val > 3 {
-          safe -= 1
-          break
-        }  
       }
     }
-
-    safe += 1
   }
   
   // part 1
   fmt.Println("===", safe, "safe reports")
 
-  // TODO: part 2
+  // part 2
+  safe_dampened += safe
+  fmt.Println("===", safe_dampened, "safe dampened reports")
 }
 
